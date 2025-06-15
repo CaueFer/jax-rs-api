4a391313af079b8b1c6721a29c7eb5f7fb1adf0e
cauefer <cauefernandescaetano@gmail.com>
2025-06-15 16:22:25 -0300

    `init`: First files structure and User entity

---
+++ src/main/java/org/example/user/domain/model/User.java
@@ -0,0 +1,25 @@
+package org.example.user.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID id;
+
+    String name;
+
+    String email;
+
+    String password;
+}
